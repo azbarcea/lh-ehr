35476a445e5803ca845785f38ac92c22e3b1025e
Alexandru Zbarcea <alexz@apache.org>
2018-10-24 20:58:08 -0400

    add healthcheck for db container

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -42,23 +42,24 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
 	if [ ! -e .htaccess ]; then
 		# NOTE: The "Indexes" option is disabled in the php:apache base image
-		cat > .htaccess <<-'EOF'
-			# BEGIN LibreHealth
-			<IfModule mod_rewrite.c>
-				RewriteEngine On
-				RewriteBase /
-				RewriteRule ^index\.php$ - [L]
-				RewriteCond %{REQUEST_FILENAME} !-f
-				RewriteCond %{REQUEST_FILENAME} !-d
-				RewriteRule . /index.php [L]
-			</IfModule>
-			# END LibreHealth
-		EOF
+		cat > .htaccess <<- EOF
+    # BEGIN LibreHealth
+    <IfModule mod_rewrite.c>
+        RewriteEngine On
+        RewriteBase /
+        RewriteRule ^index\.php$ - [L]
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteCond %{REQUEST_FILENAME} !-d
+        RewriteRule . /index.php [L]
+    </IfModule>
+    # END LibreHealth
+EOF
 		chown "$user:$group" .htaccess
 	fi
 
 	envs=(
 		LIBREHEALTH_DB_HOST
+		LIBREHEALTH_DB_PORT
 		LIBREHEALTH_DB_USER
 		LIBREHEALTH_DB_PASSWORD
 		LIBREHEALTH_DB_NAME
@@ -71,6 +72,18 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 			haveConfig=1
 		fi
 	done
+
+	# Control startup order when not using
+	#  $ docker run --health-cmd='mysqladmin ping -h"${LIBREHEALTH_DB_HOST}" --silent (...)'
+	#  nor health service
+	#  more: https://docs.docker.com/compose/startup-order/
+	#
+	#  This was an express request from community:
+	while ! mysqladmin ping -h"${LIBREHEALTH_DB_HOST}" -u"${LIBREHEALTH_DB_USER}" -p"${LIBREHEALTH_DB_PASSWORD}" --silent; do
+		>&2 echo "MYSQL host ${LIBREHEALTH_DB_HOST} is unavailable - sleeping ..."
+		sleep 1
+	done
+	>&2 echo "DB is up - continue ..."
 
 	# now that we're definitely done writing configuration, let's clear out the relevant envrionment variables (so that stray "phpinfo()" calls don't leak secrets from our code)
 	for e in "${envs[@]}"; do
